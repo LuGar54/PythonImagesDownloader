bd0a285fe9ba0cf6973203d20685a50aab574293
Ludovic Garon <ludovicgaron54@gmail.com>
2025-07-19 16:40:02 -0400

    added downloader for images of one page

---
+++ downloadImagesFromURL.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from os.path  import basename, join
+import requests
+
+base_url = "https://www.combatcamera.forces.gc.ca/site/"
+page_url = "index-eng.asp"
+page = urlopen(base_url + page_url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+images = soup.find_all("img")
+
+print(images)
+for image in images:
+    lnk = base_url + image["src"]
+    with open('images/' + basename(lnk), "wb") as f:
+        f.write(requests.get(lnk).content)
